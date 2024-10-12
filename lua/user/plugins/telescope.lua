79848d6bbe326d340904a60a0d8ea78f0aeb9fc4
KorvinEC <maksheev777@mail.ru>
2024-10-12 18:33:02 +0500

    feat(telescope): add fzf

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -28,6 +28,13 @@ return {
                 },
             },
             extensions = {
+                fzf = {
+                    fuzzy = true,                    -- false will only do exact matching
+                    override_generic_sorter = true,  -- override the generic sorter
+                    override_file_sorter = true,     -- override the file sorter
+                    case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                                     -- the default case_mode is "smart_case"
+                },
                 undo = {
                     use_delta = true,
                     use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
@@ -54,9 +61,8 @@ return {
                         },
                     },
                 }
-                }
             }
-        )
+        })
 
         telescope.load_extension("fzf")
         telescope.load_extension("undo")
