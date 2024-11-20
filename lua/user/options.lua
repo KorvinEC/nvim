2ab8c1010f754419f1bfe21dfaa045f19f4be8f4
KorvinEC <maksheev777@mail.ru>
2024-11-20 16:53:40 +0500

    feat(options.lua): remove wsl clipboard

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -29,20 +29,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 opt.clipboard = "unnamedplus"
 
-vim.g.clipboard = {
-    name = 'WslClipboard',
-    copy = {
-        ['+'] =  'clip.exe',
-        ['*'] =  'clip.exe',
-    },
-    paste = {
-        ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-}
-
-
 opt.hlsearch = true
 opt.incsearch = true
 
