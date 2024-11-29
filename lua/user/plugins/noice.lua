4ad6192608f0cedf425b52fc61313437b092b07b
KorvinEC <maksheev777@mail.ru>
2024-11-29 14:54:58 +0500

    feat(noice): move to botton and remove focusable

--- lua/user/plugins/noice.lua
+++ lua/user/plugins/noice.lua
@@ -10,6 +10,21 @@ return {
         -- OPTIONAL:
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
-    }
+        {
+            "rcarriga/nvim-notify",
+            name = "notify",
+            opts = {
+                top_down = false,
+                stages = "static",
+                -- stylua: ignore
+                max_height = function() return math.floor(vim.o.lines * 0.75) end,
+                -- stylua: ignore
+                max_width = function() return math.floor(vim.o.columns * 0.75) end,
+                on_open = function(win)
+                    vim.api.nvim_win_set_config(win, { focusable = false })
+                end,
+            },
+        },
+    },
 }
+
