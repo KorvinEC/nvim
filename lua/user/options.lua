62cd8003cd97bb8491cd8e185fd8ad161017f58f
KorvinEC <maksheev777@mail.ru>
2024-12-05 21:47:16 +0500

    feat(options): add 2 spaces for tab for frontend

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -9,6 +9,15 @@ opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true
 
+-- Override for frontend development filetypes
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {"javascript", "javascriptreact","typescript", "typescriptreact", "html", "css", "json"},
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
+})
+
 opt.wrap = false
 
 opt.ignorecase = true
