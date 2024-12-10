03607770fc94e9726e5b98bc2045961ef4a02c04
KorvinEC <maksheev777@mail.ru>
2024-12-10 20:19:26 +0500

    feat(cmp): add snippets

--- lua/user/plugins/nvim-cmp.lua
+++ lua/user/plugins/nvim-cmp.lua
@@ -1,6 +1,6 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
@@ -9,13 +9,16 @@ return {
         {
             "L3MON4D3/LuaSnip",
             version = "v2.*",
-            build = "make install_jsregexp"
+            build = "make install_jsregexp",
+            dependencies = {
+
+                { "rafamadriz/friendly-snippets" },
+            },
         },
+        "onsails/lspkind.nvim"
     },
     config = function()
         local cmp = require("cmp")
-
-        local luasnip = require("luasnip")
 
         cmp.setup.cmdline("/", {
             mapping = cmp.mapping.preset.cmdline(),
@@ -39,6 +42,10 @@ return {
                 }),
         })
 
+        local luasnip = require("luasnip")
+
+        local lspkind = require('lspkind')
+
         cmp.setup({
             completion = {
                 completeopt = "menu,menuone,preview,noselect",
@@ -59,15 +66,68 @@ return {
                 ['<C-d>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                ['<ESC>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        if luasnip.expandable() then
+                            luasnip.expand()
+                        else
+                            cmp.confirm({
+                                select = true,
+                            })
+                        end
+                    else
+                        fallback()
+                    end
+                end),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.locally_jumpable(1) then
+                        luasnip.jump(1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' },
                 { name = 'buffer' },
                 { name = 'path' },
-            })
+            }),
+            formatting = {
+                format = lspkind.cmp_format({
+                    mode = 'symbol_text', -- show only symbol annotations
+                    maxwidth = {
+                        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                        -- can also be a function to dynamically calculate max width such as
+                        -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+                        menu = 50, -- leading text (labelDetails)
+                        abbr = 50, -- actual suggestion item
+                    },
+                    ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+                    menu = {
+                        buffer = "[buff]",
+                        nvim_lsp = "[lsp]",
+                        path = "[path]",
+                        luasnip = "[snip]"
+                    }
+                })
+            },
+            experimental = { ghost_text = true }
         })
+
+        require("luasnip.loaders.from_vscode").lazy_load()
     end,
 }
