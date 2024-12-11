aa6e4f4adf5f3a8ae39c5ef35aba3fbbea459b58
KorvinEC <maksheev777@mail.ru>
2024-12-11 13:36:23 +0500

    feat(telescope): update usage

--- lua/user/plugins/lsp/lsp_config.lua
+++ lua/user/plugins/lsp/lsp_config.lua
@@ -22,22 +22,23 @@ return {
                 -- Buffer local mappings.
                 -- See `:help vim.lsp.*` for documentation on any of the below functions
                 local opts = { buffer = ev.buf, silent = true }
+                local builtin = require('telescope.builtin')
 
                 -- set keybinds
                 opts.desc = "Show LSP references"
-                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+                keymap.set("n", "gR", builtin.lsp_references, opts) -- show definition, references
 
                 opts.desc = "Go to declaration"
                 keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
                 opts.desc = "Show LSP definitions"
-                keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+                keymap.set("n", "gd", builtin.lsp_definitions, opts) -- show lsp definitions
 
                 opts.desc = "Show LSP implementations"
-                keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+                keymap.set("n", "gi", builtin.lsp_implementations, opts) -- show lsp implementations
 
                 opts.desc = "Show LSP type definitions"
-                keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+                keymap.set("n", "gt", builtin.lsp_type_definitions, opts) -- show lsp type definitions
 
                 opts.desc = "See available code actions"
                 keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
