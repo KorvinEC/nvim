b3aa39a5ad2f3d0e7c79889848791665a748c582
KorvinEC <maksheev777@mail.ru>
2024-12-29 13:32:58 +0500

    feat(spider): change mappings

--- lua/user/plugins/spider.lua
+++ lua/user/plugins/spider.lua
@@ -3,9 +3,18 @@ return {
     lazy = true,
     keys = { "w", "e", "b", "ge" },
     config = function()
+        local spider = require("spider")
+
+        spider.setup {
+            skipInsignificantPunctuation = false,
+            subwordMovement = true,
+        }
+
         vim.keymap.set({ "n", "o", "x" }, "W", "w", { desc = "Normal w" })
         vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+        vim.keymap.set({ "n", "o", "x" }, "E", "e", { desc = "Normal e" })
         vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+        vim.keymap.set({ "n", "o", "x" }, "B", "b", { desc = "Spider-b" })
         vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
         vim.keymap.set(
             { "n", "o", "x" },
