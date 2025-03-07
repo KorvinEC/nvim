a26fb7d86373d6a5d4052ccf58fc7e48e142b65b
KorvinEC <maksheev777@mail.ru>
2025-03-07 15:10:10 +0500

    feat(keymap): remove buffer mapping

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -5,12 +5,5 @@ local keymap = vim.keymap
 keymap.set("n", "<C-_>", "gcc", { remap = true })
 keymap.set("v", "<C-_>", "gcc", { remap = true })
 
-keymap.set("n", "<C-b>k", "<cmd>bnext<CR>")
-keymap.set("n", "<C-b>j", "<cmd>bprevious<CR>")
-keymap.set("n", "<C-b>h", "<cmd>bfirst<CR>")
-keymap.set("n", "<C-b>l", "<cmd>blast<CR>")
-keymap.set("n", "<C-b>s", "<cmd>ls<CR>")
-keymap.set("n", "<C-b>x", "<cmd>bdelete<CR>")
-
 keymap.set("n", "<leader>ol", "<cmd>Lazy<CR>", { desc = "Open Lazy" })
 keymap.set("n", "<leader>om", "<cmd>Mason<CR>", { desc = "Open Mason" })
