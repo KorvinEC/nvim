1fbd7e622e6ec99b7b468b1452ba35256978f6c4
KorvinEC <maksheev777@mail.ru>
2025-03-07 15:10:32 +0500

    feat(keymap): remove comment mapping

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -2,8 +2,5 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("n", "<C-_>", "gcc", { remap = true })
-keymap.set("v", "<C-_>", "gcc", { remap = true })
-
 keymap.set("n", "<leader>ol", "<cmd>Lazy<CR>", { desc = "Open Lazy" })
 keymap.set("n", "<leader>om", "<cmd>Mason<CR>", { desc = "Open Mason" })
