97042388470d15a0bc8a4b0e99aaf050791e50ff
KorvinEC <maksheev777@mail.ru>
2025-03-30 16:00:40 +0500

    feat: add undo file

--- lua/user/plugins/undotree.lua
+++ lua/user/plugins/undotree.lua
@@ -2,5 +2,6 @@ return {
   "mbbill/undotree",
   config = function()
     vim.keymap.set("n", "<leader>ou", vim.cmd.UndotreeToggle, { desc = "Open UndoTree" })
+    vim.opt.undofile = true
   end
 }
