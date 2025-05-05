5634fcae0588bd513268ca8aae11b7339f297420
KorvinEC <maksheev777@mail.ru>
2025-05-05 20:28:13 +0500

    feat(keymap): add write file shortcut

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -4,3 +4,7 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader>ol", "<cmd>Lazy<CR>", { desc = "Open Lazy" })
 keymap.set("n", "<leader>om", "<cmd>Mason<CR>", { desc = "Open Mason" })
+
+
+keymap.set("n", "<leader>w", "<cmd>write<CR>", { desc = "Write file" })
+keymap.set("n", "<leader>W", "<cmd>write!<CR>", { desc = "Force write file" })
