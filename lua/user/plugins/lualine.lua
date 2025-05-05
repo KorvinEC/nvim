94ee1aa4c180216f68b35e29720bfb8341fa8736
KorvinEC <maksheev777@mail.ru>
2025-05-05 21:45:43 +0500

    feat(lualine): format remove top bar

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -13,8 +13,8 @@ return {
         lualine_c = {
           {
             'filename',
-            file_status = true,                 -- Displays file status (readonly status, modified status)
-            newfile_status = false,             -- Display new file status (new file means no write after created)
+            file_status = true,     -- Displays file status (readonly status, modified status)
+            newfile_status = false, -- Display new file status (new file means no write after created)
             path = 1,
             -- 0: Just the filename
             -- 1: Relative path
@@ -25,13 +25,19 @@ return {
             -- Shortens path to leave 40 spaces in the window
             -- for other components. (terrible name, any suggestions?)
             symbols = {
-              modified = '[+]',                    -- Text to show when the file is modified.
-              readonly = '[-]',                    -- Text to show when the file is non-modifiable or readonly.
-              unnamed = '[No Name]',               -- Text to show for unnamed buffers.
-              newfile = '[New]',                   -- Text to show for newly created file before first write
+              modified = '[+]',      -- Text to show when the file is modified.
+              readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+              unnamed = '[No Name]', -- Text to show for unnamed buffers.
+              newfile = '[New]',     -- Text to show for newly created file before first write
             }
           }
-        }
+        },
+        lualine_x = {
+          'lsp_status',
+          'encoding',
+          'fileformat',
+          'filetype'
+        },
       },
       inactive_sections = {
         lualine_a = { 'mode' },
@@ -40,15 +46,6 @@ return {
       },
       inactive_winbar = {
         lualine_a = { { "filename", path = 1 } },
-      },
-      tabline = {
-        lualine_a = {
-          {
-            "buffers",
-            mode = 4
-          }
-        },
-        lualine_z = { "tabs" },
       },
     })
   end
