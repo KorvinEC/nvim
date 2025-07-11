64853f0e4c7d21651b2800705d709278c4ef393a
KorvinEC <maksheev777@mail.ru>
2025-07-11 15:26:44 +0500

    feat: add dbee
    Add database ui

---
+++ lua/user/plugins/dbee.lua
@@ -0,0 +1,16 @@
+return {
+  "kndndrj/nvim-dbee",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  build = function()
+    require("dbee").install()
+  end,
+  config = function()
+    local dbee = require("dbee")
+
+    dbee.setup()
+
+    vim.keymap.set('n', '<leader>oe', dbee.toggle, { desc = "Toggle Dbee" })
+  end,
+}
