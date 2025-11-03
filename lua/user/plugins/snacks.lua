e49d51c4f7347779d9204c60378709351f483849
KorvinEC <maksheev777@mail.ru>
2025-11-03 16:11:35 +0500

    feat(noice): remove noice

--- lua/user/plugins/snacks.lua
+++ lua/user/plugins/snacks.lua
@@ -25,8 +25,26 @@ return {
           truncate = 500,
         },
       },
+      win = {
+        input = {
+          keys = {
+            ["<C-y>"] = { "confirm", mode = { "n", "i" } },
+            ["<C-w>"] = { "focus_preview", mode = { "n", "i" } },
+            ["<C-u>"] = { "preview_scroll_up", mode = { "n", "i" } },
+            ["<C-d>"] = { "preview_scroll_down", mode = { "n", "i" } },
+          }
+        },
+        list = {
+          keys = {
+            ["<C-y>"] = { "confirm", mode = { "n", "i" } },
+            ["<C-w>"] = { "focus_preview", mode = { "n", "i" } },
+            ["<C-u>"] = { "preview_scroll_up", mode = { "n", "i" } },
+            ["<C-d>"] = { "preview_scroll_down", mode = { "n", "i" } },
+          }
+        },
+      },
     },
-    notifier = { enabled = true },
+    notifier = { enabled = false },
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = {
@@ -42,26 +60,26 @@ return {
     image = { enabled = false }
   },
   keys = {
-    { "<leader>ff",  function() Snacks.picker.files({ hidden = true }) end,    desc = "Find Files" },
-    { "<leader>fs",  function() Snacks.picker.git_status() end,                desc = "Git Status" },
-    { "<leader>fb",  function() Snacks.picker.buffers({ focus = "list" }) end, desc = "Buffers" },
-    { "<leader>fn",  function() Snacks.picker.notifications() end,             desc = "Notification History" },
-    { "<leader>fh",  function() Snacks.picker.command_history() end,           desc = "Command History" },
-    { '<leader>fr"', function() Snacks.picker.registers() end,                 desc = "Registers" },
-    { "<leader>fg",  function() Snacks.picker.grep() end,                      desc = "Grep" },
-    { "<leader>fw",  function() Snacks.picker.grep_word() end,                 desc = "Visual selection or word", mode = { "n", "x" } },
-    { "<leader>f/",  function() Snacks.picker.help() end,                      desc = "Help Pages" },
-    { "<leader>fl",  function() Snacks.picker.lines() end,                     desc = "Jump to highlighted" },
+    { "<leader>ff",  function() Snacks.picker.files({ hidden = true }) end, desc = "Find Files" },
+    { "<leader>fs",  function() Snacks.picker.git_status() end,             desc = "Git Status" },
+    { "<leader>fb",  function() Snacks.picker.buffers() end,                desc = "Buffers" },
+    { "<leader>fn",  function() Snacks.picker.notifications() end,          desc = "Notification History" },
+    { "<leader>fh",  function() Snacks.picker.command_history() end,        desc = "Command History" },
+    { '<leader>fr"', function() Snacks.picker.registers() end,              desc = "Registers" },
+    { "<leader>fg",  function() Snacks.picker.grep() end,                   desc = "Grep" },
+    { "<leader>fw",  function() Snacks.picker.grep_word() end,              desc = "Visual selection or word", mode = { "n", "x" } },
+    { "<leader>f/",  function() Snacks.picker.help() end,                   desc = "Help Pages" },
+    { "<leader>fl",  function() Snacks.picker.lines() end,                  desc = "Jump to highlighted" },
 
-    { "<leader>bd",  function() Snacks.bufdelete() end,                        desc = "Delete buffer" },
-    { "<leader>bD",  function() Snacks.bufdelete.other() end,                  desc = "Delete all other buffers" },
+    { "<leader>bd",  function() Snacks.bufdelete() end,                     desc = "Delete buffer" },
+    { "<leader>bD",  function() Snacks.bufdelete.other() end,               desc = "Delete all other buffers" },
 
-    { "<leader>og",  function() Snacks.lazygit() end,                          desc = "Open Lazygit" },
+    { "<leader>og",  function() Snacks.lazygit() end,                       desc = "Open Lazygit" },
 
-    { "grr",         function() Snacks.picker.lsp_references() end,            nowait = true,                     desc = "References" },
-    { "gd",          function() Snacks.picker.lsp_definitions() end,           desc = "Goto Definition" },
-    { "gD",          function() Snacks.picker.lsp_declarations() end,          desc = "Goto Declaration" },
-    { "gi",          function() Snacks.picker.lsp_implementations() end,       desc = "Goto Implementation" },
-    { "gt",          function() Snacks.picker.lsp_type_definitions() end,      desc = "Goto T[y]pe Definition" },
+    { "grr",         function() Snacks.picker.lsp_references() end,         nowait = true,                     desc = "References" },
+    { "gd",          function() Snacks.picker.lsp_definitions() end,        desc = "Goto Definition" },
+    { "gD",          function() Snacks.picker.lsp_declarations() end,       desc = "Goto Declaration" },
+    { "gi",          function() Snacks.picker.lsp_implementations() end,    desc = "Goto Implementation" },
+    { "gt",          function() Snacks.picker.lsp_type_definitions() end,   desc = "Goto T[y]pe Definition" },
   },
 }
