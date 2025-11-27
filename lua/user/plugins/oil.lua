4c622348bb5ceccc2bf286afca5bd73f76132b9d
KorvinEC <maksheev777@mail.ru>
2025-11-27 16:07:57 +0500

    feat(oil): update view

--- lua/user/plugins/oil.lua
+++ lua/user/plugins/oil.lua
@@ -1,37 +1,42 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+    columns = {
+      "permissions",
+      "size",
+      "mtime",
+      "icon",
+    },
+    use_default_keymaps = false,
+    keymaps = {
+      ["g?"] = { "actions.show_help", mode = "n" },
+      ["<CR>"] = "actions.select",
+      ["<C-y>"] = "actions.select",
+
+      ["<leader>v"] = { "actions.select", opts = { vertical = true, split = "belowright" } },
+      ["<leader>s"] = { "actions.select", opts = { horizontal = true, split = "belowright" } },
+      ["<leader>t"] = { "actions.select", opts = { tab = true } },
+
+      ["-"] = { "actions.parent", mode = "n" },
+      ["_"] = { "actions.open_cwd", mode = "n" },
+
+      ["<C-p>"] = "actions.preview",
+
+      ["q"] = { "actions.close", mode = "n" },
+
+      ["`"] = { "actions.cd", mode = "n" },
+      ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+
+      ["gl"] = "actions.refresh",
+      ["gs"] = { "actions.change_sort", mode = "n" },
+      ["gx"] = "actions.open_external",
+      ["g."] = { "actions.toggle_hidden", mode = "n" },
+      ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },
+  },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-  config = function()
-    require("oil").setup({
-      use_default_keymaps = false,
-      keymaps = {
-        ["g?"] = { "actions.show_help", mode = "n" },
-        ["<CR>"] = "actions.select",
-        ["<C-y>"] = "actions.select",
-
-        ["<leader>v"] = { "actions.select", opts = { vertical = true, split = "belowright" } },
-        ["<leader>s"] = { "actions.select", opts = { horizontal = true, split = "belowright" } },
-        ["<leader>t"] = { "actions.select", opts = { tab = true } },
-
-        ["-"] = { "actions.parent", mode = "n" },
-        ["_"] = { "actions.open_cwd", mode = "n" },
-
-        ["<C-p>"] = "actions.preview",
-
-        ["q"] = { "actions.close", mode = "n" },
-
-        ["`"] = { "actions.cd", mode = "n" },
-        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-
-        ["gl"] = "actions.refresh",
-        ["gs"] = { "actions.change_sort", mode = "n" },
-        ["gx"] = "actions.open_external",
-        ["g."] = { "actions.toggle_hidden", mode = "n" },
-        ["g\\"] = { "actions.toggle_trash", mode = "n" },
-      },
-    })
+  init = function()
     vim.keymap.set("n", "<leader>T", "<cmd>Oil<CR>")
   end
 }
