1716bedd1a183aa30cbea44e022e772951a276bd
KorvinEC <maksheev777@mail.ru>
2026-01-21 13:37:41 +0500

    feat(molten): move to latest repo

--- lua/user/plugins/molten.lua
+++ lua/user/plugins/molten.lua
@@ -56,17 +56,14 @@ return {
     end
   },
   {
-    "KorvinEC/molten-nvim",
-    branch = "virtual_lines_focus",
+    "benlubas/molten-nvim",
     version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
     dependencies = {
-      "3rd/image.nvim",
       "quarto-dev/quarto-nvim"
     },
+    ft = { "quarto", "markdown" },
     build = ":UpdateRemotePlugins",
     init = function()
-      -- these are examples, not defaults. Please see the readme
-      vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_wrap_output = false
       vim.g.molten_virt_text_output = true
       vim.g.molten_virt_lines_off_by_1 = true
