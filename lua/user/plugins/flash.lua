ee11447e48d3dd5c1f9fb9ce7237326db62405da
KorvinEC <maksheev777@mail.ru>
2026-01-21 13:36:50 +0500

    feat(flash): new mappings

--- lua/user/plugins/flash.lua
+++ lua/user/plugins/flash.lua
@@ -3,11 +3,10 @@ return {
   event = { "VeryLazy" },
   dependencies = { "folke/snacks.nvim" },
   keys = {
-    { "<leader>j", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-    { "<leader>J", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-    { "r",         mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-    { "R",         mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>",     mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    { "<C-s>", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+    { "<C-t>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+    { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+    { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
   },
   specs = {
     {
@@ -49,12 +48,14 @@ return {
   config = function()
     local colors = require("gruvbox").palette
 
+    local flash = require("flash")
+
     vim.api.nvim_set_hl(0, "FlashLabelHighlight", {
       fg = colors.bright_aqua,
       bg = colors.dark_aqua_hard
     })
 
-    require('flash').setup({
+    flash.setup({
       label = {
         ---@type fun(opts:Flash.Format): string[][]
         format = function(opts)
