095b2ec84280f21115535fcc55021adcb79f5c0c
KorvinEC <maksheev777@mail.ru>
2026-01-21 13:37:54 +0500

    feat(smart-splits): change structure

--- lua/user/plugins/smart-splits.lua
+++ lua/user/plugins/smart-splits.lua
@@ -5,23 +5,25 @@ return {
     zellij_move_focus_or_tab = true,
   },
   init = function()
+    local sp = require("smart-splits")
+
     -- moving between splits
-    vim.keymap.set('n', '<A-h>', require('smart-splits').move_cursor_left)
-    vim.keymap.set('n', '<A-j>', require('smart-splits').move_cursor_down)
-    vim.keymap.set('n', '<A-k>', require('smart-splits').move_cursor_up)
-    vim.keymap.set('n', '<A-l>', require('smart-splits').move_cursor_right)
+    vim.keymap.set('n', '<A-h>', sp.move_cursor_left)
+    vim.keymap.set('n', '<A-j>', sp.move_cursor_down)
+    vim.keymap.set('n', '<A-k>', sp.move_cursor_up)
+    vim.keymap.set('n', '<A-l>', sp.move_cursor_right)
 
-    vim.keymap.set('n', '<A-Left>', require('smart-splits').resize_left)
-    vim.keymap.set('n', '<A-Down>', require('smart-splits').resize_down)
-    vim.keymap.set('n', '<A-Up>', require('smart-splits').resize_up)
-    vim.keymap.set('n', '<A-Right>', require('smart-splits').resize_right)
+    vim.keymap.set('n', '<A-Left>', sp.resize_left)
+    vim.keymap.set('n', '<A-Down>', sp.resize_down)
+    vim.keymap.set('n', '<A-Up>', sp.resize_up)
+    vim.keymap.set('n', '<A-Right>', sp.resize_right)
 
-    vim.keymap.set('n', '<A-\\>', require('smart-splits').move_cursor_previous)
+    vim.keymap.set('n', '<A-\\>', sp.move_cursor_previous)
 
     -- swapping buffers between windows
-    vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left, { desc = "Swap buffer to left" })
-    vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down, { desc = "Swap buffer to down" })
-    vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up, { desc = "Swap buffer to up" })
-    vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right, { desc = "Swap buffer to right" })
+    vim.keymap.set('n', '<leader><leader>h', sp.swap_buf_left, { desc = "Swap buffer to left" })
+    vim.keymap.set('n', '<leader><leader>j', sp.swap_buf_down, { desc = "Swap buffer to down" })
+    vim.keymap.set('n', '<leader><leader>k', sp.swap_buf_up, { desc = "Swap buffer to up" })
+    vim.keymap.set('n', '<leader><leader>l', sp.swap_buf_right, { desc = "Swap buffer to right" })
   end
 }
