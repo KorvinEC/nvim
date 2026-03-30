87bf3f1c7ce66e84d3596d74ed75c6955ed75492
KorvinEC <maksheev777@mail.ru>
2026-03-30 21:17:14 +0500

    feat(lsp): updates

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -16,6 +16,18 @@ return {
     Lua = {
       hint = {
         enable = true,
+      },
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = {
+          "vim",
+          "require"
+        }
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true)
       }
     }
   }
