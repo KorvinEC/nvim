d120268672736037963ce40c2703485b07d24cb8
KorvinEC <maksheev777@mail.ru>
2026-04-18 18:28:50 +0500

    fix(highligh-colors): add virtual symbol

--- lua/plugins/highlight-colors.lua
+++ lua/plugins/highlight-colors.lua
@@ -4,8 +4,9 @@ vim.pack.add({
 
 require("nvim-highlight-colors").setup({
   render = "virtual",
-  virtual_symbol_position = "eow",
-  virtual_symbol = "",
-  virtual_symbol_prefix = " ",
-  virtual_symbol_suffix = " ",
+  virtual_symbol_position = 'eow',
+  virtual_symbol = '',
+  virtual_symbol_prefix = ' ',
+  virtual_symbol_suffix = ' ',
+  exclude_filetypes = { "lazy" }
 })
